9e50a4fdb1464ccc6692644ef2f0db9dc173748b
AlexPan <ppgod@live.cn>
2017-05-10 20:51:39 +0800

    implement manual product quantity input

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,30 +21,66 @@
 $(document).on('turbolinks:load', function() {
   $('.dropdown').hover(function() {
       $(this).addClass('open');
-  },
-  function() {
+    },
+    function() {
       $(this).removeClass('open');
+    });
+
+  /*只能输入数字*/
+  $('.quantity-input').on('input', function(event) {
+    var max = parseToInt($('.product-quantity').html());
+    var value = parseToInt($(this).val());
+    if (value <= 1) {
+      $(this).val('1');
+      $("#quantity-minus").addClass('disabled');
+    } else {
+      $("#quantity-minus").removeClass('disabled');
+    }
+    // 限制输入数量不大于库存
+    if (value >= max) {
+      $(this).val(max);
+      $("#quantity-plus").addClass('disabled');
+    } else {
+      $(this).val(value);
+      $("#quantity-plus").removeClass('disabled');
+    }
+  }).on('blur', function(event) {
+    var value = $(this).val();
+    if ( value == '' || value == 0) {
+      $(this).val('1');
+    }
   });
 
   /*增加数量*/
-  $("#quantity-plus").click(function(event){
+  $("#quantity-plus").click(function(event) {
     event.preventDefault();
-    var num = parseInt($(".quantity-input").val()) + 1;
+    var max = parseToInt($('.product-quantity').html());
+    var value = parseInt($(".quantity-input").val()) + 1;
     $(".quantity-input").val(num);
-    if(num > 1){
-      $("#quantity-minus").removeClass("disabled");
+    $("#quantity-minus").removeClass("disabled");
+    if (value > max) {
+      $(this).val(max);
+      $("#quantity-plus").addClass('disabled');
     }
   });
 
   /*减少数量*/
-  $("#quantity-minus").click(function(event){
+  $("#quantity-minus").click(function(event) {
     event.preventDefault();
-  	var num = parseInt($(".quantity-input").val());
-  	if(num > 1){
+    var num = parseInt($(".quantity-input").val());
+    if (num > 1) {
       $(".quantity-input").val(num -= 1);
-  	}
-    if(num <= 1){
+    }
+    if (num <= 1) {
       $("#quantity-minus").addClass("disabled");
     }
   });
 });
+
+// 移除字符输入
+function parseToInt(value) {
+  while (value.match(/[^\d]/)) {
+    value = value.replace(/[^\d]/, '');
+  }
+  return parseInt(value == '' ? 0 : value);
+}
