b9bb774e0a59b50b81c0cd4bab45b285dcf85fee
AlexPan <ppgod@live.cn>
2017-05-15 17:48:52 +0800

    implement cart items count effect

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,11 +1,11 @@
 module FlashesHelper
-  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning" }.freeze
+  FLASH_CLASSES = { alert: "danger", notice: "success", info: "info", warning: "warning" }.freeze
 
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
   end
 
   def user_facing_flashes
-    flash.to_hash.slice "alert", "notice", "warning"
+    flash.to_hash.slice "alert", "notice", "info", "warning"
   end
 end
