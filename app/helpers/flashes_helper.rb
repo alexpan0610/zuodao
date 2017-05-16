9ce7b99f4f65c75605ce1a12ab2f68d062a9e1ba
AlexPan <ppgod@live.cn>
2017-05-16 12:21:04 +0800

    change alert style

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -8,4 +8,17 @@ module FlashesHelper
   def user_facing_flashes
     flash.to_hash.slice "alert", "notice", "info", "warning"
   end
+
+  def render_alert_icon(key)
+    case flash_class(key)
+    when "info"
+      content_tag(:i, "info_outline", class: "material-icons")
+    when "success"
+      content_tag(:i, "check", class: "material-icons")
+    when "danger"
+      content_tag(:i, "error_outline", class: "material-icons")
+    when "warning"
+      content_tag(:i, "warning", class: "material-icons")
+    end
+  end
 end
