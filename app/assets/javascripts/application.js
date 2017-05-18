ae02e08b80202668c50e6ea44093002265b7767b
刘传 <yangtuolc@gmail.com>
2017-05-18 20:25:07 +0800

    landingpage content end.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,24 @@ $(document).on('turbolinks:load', function() {
       $(this).removeClass('open');
     });
 
+
+//for progress-bar
+
+$(function () {
+$('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+});
+
+// $( window ).scroll(function() {
+// if($( window ).scrollTop() > 10){  // scroll down abit and get the action
+$(".progress-bar").each(function(){
+each_bar_width = $(this).attr('aria-valuenow');
+$(this).width(each_bar_width + '%');
+});
+
+//  }
+// });
+
+
   /*商品数量输入控制*/
   $('#quantity-input').on('input', function(e) {
     var max = parseToInt($('.product-quantity').html());
